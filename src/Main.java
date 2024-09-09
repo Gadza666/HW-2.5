98ec062614b9ce16eef28aa309fe49fbd135f45f
Gadza88 <vasiliew.renat@gmail.com>
2024-09-09 23:55:11 +0300

    fix done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import exeption.WrongLoginException;
+import exeption.WrongPasswordException;
 import service.ValidationService;
 
 import static service.ValidationService.validate;
@@ -5,9 +7,16 @@ import static service.ValidationService.validate;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        validate("G_12323", "55566666333", "55566666333");
-        System.out.println("Conform");
-    }
+        try {
 
+            System.out.println("Hello world!");
+            validate("G_12323", "55566666333", "55566666333");
+            System.out.println("Conformity");
+        } catch (WrongPasswordException a) {
+            System.out.println("Not conformity password");
+        } catch (WrongLoginException b) {
+            System.out.println(" Not conformity login");
+        }
+
+    }
 }
